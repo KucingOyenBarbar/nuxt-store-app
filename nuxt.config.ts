2b92f0fe24091affff3ad8f2b08b2228e91e3d41
Farhan <farhan18apr02@gmail.com>
2023-08-31 23:49:57 +0700

    add dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/web-vitals',
+    '@nuxtjs/robots',
+  ],
+
+  webVitals: {
+    provider: 'auto',
+    debug: false, // debug enable metrics reporting on dev environments
+    disabled: false,
+  },
+
+  googleFonts: {
+    families: {
+      Roboto: true,
+      Inter: [400, 700],
+      'Josefin+Sans': true,
+      Lato: [100, 300],
+      Raleway: {
+        wght: [100, 400],
+        ital: [100],
+      },
+    },
+
+    preconnect: true,
+    prefetch: true,
+    preload: true,
+  },
+});
